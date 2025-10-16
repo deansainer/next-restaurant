7d7e159d93e3a9cfece142c0fc2f5ae0095fed52
deansainer <determind2k@gmail.com>
2025-10-16 21:44:47 +0200

    created main homepage components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import Slider from "@/components/Slider";
+import Featured from "@/components/Featured";
+import Offer from "@/components/Offer";
+
+
 
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <main>
+      <Slider/>
+      <Featured/>
+      <Offer/>
+    </main>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-gray-800 flex justify-between items-center'>
+      <Link href='/' className='font-bold tex-xl'>Massimo</Link>
+      <p>All rights reserved.</p>
+    </div>
   )
 }
 
