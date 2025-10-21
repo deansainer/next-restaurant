a01c2d7a9206411433d088a0f47191b954dae27c
deansainer <determind2k@gmail.com>
2025-10-21 21:20:15 +0200

    added menu page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { menu } from '@/data'
+import Link from 'next/link'
+
 
 const MenuPage = () => {
   return (
-    <div>Menu Page</div>
+    <div className='p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center justify-center'>
+      {menu.map((category) => (
+        <Link href={`/menu/${category.slug}`} key={category.id} className='w-full h-1/3 bg-cover p-8 md:h-1/2' style={{backgroundImage: `url(${category.img})`}}>
+          
+          <div className={`text-${category.color} w-1/2`}>
+            <h1 className='uppercase font-bold text-3xl'>{category.title}</h1>
+            <p className='text-sm my-8'>{category.desc}</p>
+            <button className='hidden md:block bg-gray-800 py-2 px-4 hover:bg-gray-700 mt-5 cursor-pointer rounded-xl text-white transition-all duration-500'>Submit</button>
+          </div>
+        </Link>
+      ))}
+    </div>
   )
 }
 
