566fcfda0fe891b5ac22a22d34f0d3a5f01ba16f
deansainer <determind2k@gmail.com>
2025-10-26 16:34:03 +0100

    implemented orders page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -2,7 +2,47 @@ import React from 'react'
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div className='p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-12rem)]'>
+      <table className='w-full border-separate border-spacing-3'>
+        <thead>
+          <tr className='text-left'>
+            <th className='hidden md:block'>Order ID</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th className='hidden md:block'>Products</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+          <tr className='text-sm md:text-base bg-red-100'>
+            <td className='hidden md:block py-6 px-1'>12412</td>
+            <td className='py-6 px-1'>19.10.2025</td>
+            <td className='py-6 px-1'>$54.99</td>
+            <td className='hidden md:block py-6 px-1'>Pizza Salami, Coca Cola 1L, Burger Menu</td>
+            <td className='py-6 px-1'>On the way (approx. 10min)</td>
+          </tr>
+
+          <tr className='text-sm md:text-base odd:bg-gray-100'>
+            <td className='hidden md:block py-6 px-1'>12412</td>
+            <td className='py-6 px-1'>19.10.2025</td>
+            <td className='py-6 px-1'>$44.99</td>
+            <td className='hidden md:block py-6 px-1'>Pizza Salami, Coca Cola 1L, Burger Menu</td>
+            <td className='py-6 px-1'>Delivered</td>
+          </tr>
+
+          <tr className='text-sm md:text-base odd:bg-gray-100'>
+            <td className='hidden md:block py-6 px-1'>12412</td>
+            <td className='py-6 px-1'>19.10.2025</td>
+            <td className='py-6 px-1'>$78.99</td>
+            <td className='hidden md:block py-6 px-1'>Pizza Salami, Coca Cola 1L, Burger Menu</td>
+            <td className='py-6 px-1'>Delivered</td>
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
   )
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-gray-800 flex justify-between items-center'>
+    <div className='h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-gray-800 flex justify-between items-center'>
       <Link href='/' className='font-bold tex-xl'>Massimo</Link>
       <p>All rights reserved.</p>
     </div>
